736dff1f3e24c46fd81284ccfc923eb77894e716
Hiep Nguyen <hiepnguyentpc@gmail.com>
2022-03-15 20:18:03 -0500

    Coin Item Created

---
+++ src/components/CoinItem/index.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {Text, View, Image } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import styles from './styles';
+
+const CoinItem = () => {
+    return (
+        <View style={styles.coinContainer}>
+        <Image
+          source={{
+            url: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
+          }}
+          style={{
+            height: 30,
+            width: 30,
+            marginRight: 10,
+            alignSelf: "center",
+          }}
+        />
+
+        <View>
+          <Text style={styles.title}>Bitcoin</Text>
+
+          <View style={{ flexDirection: "row" }}>
+            <View style = {styles.rankContainer}>
+              <Text style={styles.rank}>1</Text>
+            </View>
+            <Text style={styles.text}>BTC</Text>
+            <AntDesign
+              name="caretdown"
+              size={15}
+              color="red"
+              style={{ alignSelf: "center", marginRight: 5 }}
+            />
+            <Text style={styles.text}>0.65%</Text>
+          </View>
+        </View>
+        <View style={{ marginLeft: "auto" }}>
+          <Text style={styles.title}>56.265,09</Text>
+          <Text style={styles.text}>MCap 1.067 T</Text>
+        </View>
+      </View>
+    );
+}
+
+export default CoinItem;
